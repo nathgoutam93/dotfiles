1e85b029e5a83aceffa011b7cbaa2dcf26dcb96e
nathgoutam93 <nathgoutam93@gmail.com>
2025-12-24 02:14:27 +0530

    headers can now be manually switched

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -5,38 +5,52 @@ return {
 		config = function()
 			local alpha = require("alpha")
 			local dashboard = require("alpha.themes.dashboard")
-
 			local headers = require("headers")
-			math.randomseed(os.time())
-			local header = headers[math.random(#headers)]
 
-			dashboard.section.header.val = header
+			-- ring iterator state
+			local idx = 0
+			local total = #headers
+
+			local function next_header()
+				idx = (idx % total) + 1
+				return headers[idx]
+			end
+
+			-- initial header
+			dashboard.section.header.val = next_header()
 			dashboard.section.header.opts.hl = "AlphaHeader"
 			dashboard.section.buttons.val = {}
 			dashboard.section.footer.val = "MOMO AYASE"
+
 			alpha.setup(dashboard.opts)
 
+			-- alpha buffer keymap
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "alpha",
+				callback = function(ev)
+					vim.keymap.set("n", "<leader>ah", function()
+						dashboard.section.header.val = next_header()
+						vim.cmd("Alpha")
+					end, { buffer = ev.buf })
+				end,
+			})
+
+			-- reopen Alpha after :bd
 			vim.api.nvim_create_autocmd("BufEnter", {
 				callback = function()
-					local buf = vim.api.nvim_get_current_buf()
-
-					if vim.api.nvim_buf_get_name(buf) ~= "" then
+					if vim.bo.filetype == "alpha" then
 						return
 					end
 
+					local buf = vim.api.nvim_get_current_buf()
+					if vim.api.nvim_buf_get_name(buf) ~= "" then
+						return
+					end
 					if vim.bo[buf].buftype ~= "" then
 						return
 					end
 
-					if vim.api.nvim_buf_line_count(buf) ~= 1 then
-						return
-					end
-
-					if vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1] ~= "" then
-						return
-					end
-
-					dashboard.section.header.val = headers[math.random(#headers)]
+					dashboard.section.header.val = next_header()
 					vim.cmd("Alpha")
 				end,
 			})
